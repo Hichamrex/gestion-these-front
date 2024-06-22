343b194aea0bfaa29555eccffea8e31922be1722
hichamrex <laqrafi99@gmail.com>
2024-06-22 02:06:35 +0100

    Add new features

---
+++ src/redux/features/Theses/sliceThese.ts
@@ -0,0 +1,58 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { apiSlice } from "../../Api/apiSlice";
+
+
+const sliceThese = apiSlice.injectEndpoints({
+    endpoints: (build) => ({
+      getTheses: build.query({
+        query: () => ({
+          url: `/theses`,
+        }),
+        providesTags: ['Theses'] 
+      }),
+      getTheseUser: build.query({
+        query: (userId) => ({
+          url: `/user/${userId}/theses`,
+        }),
+        providesTags: ['Theses'] 
+      }),
+      getThese: build.query({
+        query: thesesId => ({
+          url: `/these/${thesesId}`,
+        }),
+        providesTags: ['These'] 
+      }),
+      addtheses: build.mutation({
+        query: newDpart => ({
+            url: '/theses',
+            method: 'POST',
+            body: newDpart,
+        }),
+        invalidatesTags: ['Theses', 'These']
+    }),
+    edittheses: build.mutation({
+        query: newtheses => ({
+          url: `/these/${newtheses.id}`,
+          method: 'PUT',
+          body: newtheses,
+        }),
+        invalidatesTags: ['Theses', 'These']
+      }),
+      deletetheses: build.mutation({
+        query: (id) => ({
+          url: `/these/${id}`,
+          method: 'DELETE',
+        }),
+        invalidatesTags: ['Theses', 'These']
+      })
+    })    
+});
+
+export const { 
+    useAddthesesMutation,
+    useGetTheseUserQuery,
+    useGetTheseQuery,
+    useGetThesesQuery,
+    useEditthesesMutation,
+    useDeletethesesMutation
+} = sliceThese;
